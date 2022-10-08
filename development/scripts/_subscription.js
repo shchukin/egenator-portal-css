769ece75d3643170fc1c587939811f43bc46f5f2
Anton Shchukin <a.a.shchukin@gmail.com>
2022-10-09 01:09:22 +0700

    join

---
+++ development/scripts/_subscription.js
@@ -0,0 +1,15 @@
+(function($) {
+    $('.subscription__submit .button').on('click', function () {
+        var $this = $(this);
+        if ( ! $this.hasClass('button--loading') &&  ! $this.hasClass('button--success') ) {
+            $this.addClass('button--loading');
+            setTimeout(function () {
+                $this.removeClass('button--loading').addClass('button--success');
+                $('.alert').fadeIn(200);
+            }, 3000);
+            setTimeout(function () {
+                $this.removeClass('button--success');
+            }, 4500);
+        }
+    });
+})(jQuery);
